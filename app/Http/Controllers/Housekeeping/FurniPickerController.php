100770073a3801a2929bb46fe9ba5ca3b95fa663
jeanropke <jeanropke@ymail.com>
2025-03-31 16:22:20 -0300

    Clean up

--- app/Http/Controllers/Housekeeping/FurniPickerController.php
+++ app/Http/Controllers/Housekeeping/FurniPickerController.php
@@ -4,11 +4,7 @@ namespace App\Http\Controllers\Housekeeping;
 
 use App\Http\Controllers\Controller;
 use App\Models\Catalogue\CatalogueItem;
-use App\Models\Catalogue\CataloguePage;
-use App\Models\Furniture;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 
 class FurniPickerController extends Controller
 {
