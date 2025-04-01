b90f2fe0318bbd6943ba402368e870cb429db81e
jeanropke <jeanropke@ymail.com>
2025-03-31 23:57:09 -0300

    Added item offer function

--- app/Models/ItemOffer.php
+++ app/Models/ItemOffer.php
@@ -8,9 +8,18 @@ class ItemOffer extends Model
 {
     protected $table = 'cms_items_offers';
 
-    protected $fillable = [
-        'id', 'salecode', 'items', 'price', 'enabled'
-    ];
+    protected $fillable = ['id', 'salecode', 'items', 'price', 'enabled'];
 
     public $timestamps = false;
+
+    public function isValid()
+    {
+        $itemsIds = explode(',', $this->items);
+        foreach ($itemsIds as $itemId) {
+            $item = ItemDefination::find($itemId);
+            if (!$item)
+                return false;
+        }
+        return true;
+    }
 }
