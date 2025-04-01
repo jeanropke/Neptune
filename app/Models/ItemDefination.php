b046f22507e2bab4e4cab38f6e55c75e921a9a41
jeanropke <jeanropke@ymail.com>
2025-03-31 23:56:52 -0300

    Create ItemDefination.php

---
+++ app/Models/ItemDefination.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ItemDefination extends Model
+{
+    protected $table = 'items_definitions';
+
+    protected $fillable = [
+        'id ', 'sprite', 'sprite_id', 'name', 'description', 'colour', 'length', 'width', 'top_height', 'max_status', 'behaviour', 'interactor', 'is_tradable', 'is_recyclable', 'drink_ids'
+    ];
+
+    public function getNormalizedName()
+    {
+        return str_replace('*', '_', $this->sprite);
+    }
+}
