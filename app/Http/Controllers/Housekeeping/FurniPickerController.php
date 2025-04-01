ede9190149c71a5c35bb2bf0ea0d4b885b7ca552
jeanropke <jeanropke@ymail.com>
2025-04-01 12:40:31 -0300

    Moved furnipicker to ajax folder

--- app/Http/Controllers/Housekeeping/FurniPickerController.php
+++ app/Http/Controllers/Housekeeping/FurniPickerController.php
@@ -10,7 +10,7 @@ class FurniPickerController extends Controller
 {
     public function listing()
     {
-        return view('housekeeping.furnipicker.listing');
+        return view('housekeeping.ajax.furnipicker.listing');
     }
 
     public function search(Request $request)
@@ -19,6 +19,6 @@ class FurniPickerController extends Controller
             return '<p>The furni must be at least 3 characters.</p>';
 
         $items = CatalogueItem::where([['sale_code', 'LIKE', "%{$request->furni}%"]])->orWhere([['name', 'LIKE', "%{$request->furni}%"]])->get();
-        return view('housekeeping.furnipicker.search')->with('items', $items);
+        return view('housekeeping.ajax.furnipicker.search')->with('items', $items);
     }
 }
