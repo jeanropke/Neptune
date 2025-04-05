dd095d18dff7ded5c3a472beafc7c8fcc5a060d3
jeanropke <jeanropke@ymail.com>
2025-04-05 01:34:18 -0300

    Fixed figure

--- resources/views/home/widgets/ajax/guestbook/preview.blade.php
+++ resources/views/home/widgets/ajax/guestbook/preview.blade.php
@@ -1,16 +1,17 @@
 <ul class="guestbook-entries">
     <li id="guestbook-entry--1" class="guestbook-entry">
         <div class="guestbook-author">
-            <img src="{{ cms_config('site.avatarimage.url') }}?figure={{ Auth::user()->look }}&direction=2&head_direction=2&gesture=sml&size=n" alt="{{ Auth::user()->username }}" title="{{ Auth::user()->username }}" />
+            <img src="{{ cms_config('site.avatarimage.url') }}{{ user()->figure }}&direction=2&head_direction=2&gesture=sml&size=s" alt="{{ user()->username }}"
+                title="{{ user()->username }}" />
         </div>
         <div class="guestbook-message">
-            <div class="{{ Auth::user()->isOnline() ? 'online' : 'offline' }}">
-                <a href="{{ url('/') }}/home/{{ Auth::user()->username }}">
-                    {{ Auth::user()->username }}
+            <div class="{{ user()->isOnline() ? 'online' : 'offline' }}">
+                <a href="{{ url('/') }}/home/{{ user()->username }}">
+                    {{ user()->username }}
                 </a>
             </div>
             <p>
-                {!! $message  !!}
+                {!! $message !!}
             </p>
         </div>
         <div class="guestbook-cleaner">&nbsp;</div>
@@ -18,7 +19,6 @@
     </li>
 </ul>
 <div class="guestbook-toolbar clearfix">
-        
     <a href="#" class="new-button" id="guestbook-form-continue"><b>Editar</b><i></i></a>
-    <a href="#" class="new-button" id="guestbook-form-post"><b>Salvar</b><i></i></a>	
+    <a href="#" class="new-button" id="guestbook-form-post"><b>Salvar</b><i></i></a>
 </div>
