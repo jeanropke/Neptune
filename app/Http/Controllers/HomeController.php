909e97c993b9a36a3a0f176ae929731683393f27
jeanropke <jeanropke@ymail.com>
2025-06-09 01:01:45 -0300

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -221,10 +221,12 @@ class HomeController extends Controller
                 return;
 
             $bgInUse = HomeItem::where([['data', 'background'], ['home_id', $session->user_id]])->first();
-            $bgInUse->update([
-                'home_id'   => null,
-                'group_id'  => null
-            ]);
+            if($bgInUse) {
+                $bgInUse->update([
+                    'home_id'   => null,
+                    'group_id'  => null
+                ]);
+            }
             $home = HomeItem::where([['data', 'background'], ['owner_id', $session->user_id], ['item_id', $storeItem->id]])->first();
             $home->update([
                 'home_id'   => $session->home_id,
