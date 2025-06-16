6b3566c589be6298e7e69047189cc800345c7ddf
jeanropke <jeanropke@ymail.com>
2025-06-16 11:55:56 -0300

    Changed highscore widget

---
+++ resources/views/home/widgets/userexpswidget.blade.php
@@ -0,0 +1,92 @@
+<div class="movable widget HighScoresWidget" id="widget-{{ $item->id }}"
+    style="  left: {{ $item->x }}px; top: {{ $item->y }}px; z-index: {{ $item->z }};">
+    <div class="w_skin_{{ $item->skin }}">
+        <div class="widget-corner" id="widget-{{ $item->id }}-handle">
+            <div class="widget-headline">
+                <h3>@if($isEdit)<img src="{{ url('/') }}/web/images/myhabbo/icon_edit.gif" width="19" height="18"
+                        class="edit-button" id="widget-{{ $item->id }}-edit" />
+                    <script language="JavaScript" type="text/javascript">
+                        Event.observe('widget-{{ $item->id }}-edit', 'click', function(e) { openEditMenu(e, {{ $item->id }}, 'widget', 'widget-{{ $item->id }}-edit'); }, false);
+                    </script>@endif<span class="header-left">&nbsp;</span><span class="header-middle">HIGH
+                        SCORES</span><span class="header-right">&nbsp;</span>
+                </h3>
+            </div>
+        </div>
+        <div class="widget-body">
+            <div class="widget-content">
+                <table>
+                    <tbody>
+                        <tr colspan="2">
+                            <th><a href="{{ url('/') }}/games/battleball/">Battle Ball</a></th>
+                        </tr>
+                        <tr>
+                            <td>Games played</td>
+                            <td>x</td>
+                        </tr>
+                        <tr>
+                            <td>Total score</td>
+                            <td>{{ $owner->battleball_points }}</td>
+                        </tr>
+                        <tr colspan="2">
+                            <th><a href="{{ url('/') }}/games/snowstorm/">Snow Storm</a></th>
+                        </tr>
+                        <tr>
+                            <td>Games played</td>
+                            <td>x</td>
+                        </tr>
+                        <tr>
+                            <td>Total score</td>
+                            <td>{{ $owner->snowstorm_points }}</td>
+                        </tr>
+
+                        <tr colspan="2">
+                            <th><a href="{{ url('/') }}/games/wobblesquabble/">Wobble Squabble</a></th>
+                        </tr>
+                        <tr>
+                            <td>Games played</td>
+                            <td>x</td>
+                        </tr>
+                        <tr>
+                            <td>Total score</td>
+                            <td>y</td>
+                        </tr>
+                </tbody>
+
+                    {{--@if($owner->battleball_points > 0)
+                    <tr colspan="2">
+                        <th>Battleball points</th>
+                    </tr>
+                    <tr>
+                        <td>Score</td>
+                        <td>
+                            {{ $owner->battleball_points }}
+                        </td>
+                    </tr>
+                    @endif
+                    @if($owner->snowstorm_points > 0)
+                    <tr colspan="2">
+                        <th>Snowstorm points</th>
+                    </tr>
+                    <tr>
+                        <td>Score</td>
+                        <td>
+                            {{ $owner->snowstorm_points }}
+                        </td>
+                    </tr>
+                    @endif
+
+                    @if($owner->battleball_points == 0 && $owner->snowstorm_points == 0)
+                    No games scores
+                    @endif--}}
+                </table>
+
+                <script type="text/javascript">
+                    Event.onDOMReady(function() {
+                        new HighscoreListWidget({{ $item->id }}, 1, 1);
+                    });
+                </script>
+                <div class="clear"></div>
+            </div>
+        </div>
+    </div>
+</div>
