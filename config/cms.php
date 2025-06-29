31e01934dcef138eb80e46756725416e85ef5abe
jeanropke <jeanropke@ymail.com>
2025-06-29 02:24:34 -0300

    Update cms.php

--- config/cms.php
+++ config/cms.php
@@ -2,9 +2,9 @@
 
 return [
     'name'      => 'NeptuneCMS',
-    'version'   => '0.7',
+    'version'   => '0.8',
     'title'     => 'Goldfish',
     'stable'    => 'UNSTABLE',
-    'build'     => '20 Jun 2025 20:12:03',
+    'build'     => '29 Jun 2025 02:24:37',
     'gmt'       => 'GMT-3 (Brasília time)'
 ];
