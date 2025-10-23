aea8af8bdb3f4c0e43e3db981e98d5cd0d9034ce
jeanropke <jeanropke@ymail.com>
2025-10-22 22:44:31 -0300

    Update traxplayerwidget.blade.php

--- resources/views/home/widgets/traxplayerwidget.blade.php
+++ resources/views/home/widgets/traxplayerwidget.blade.php
@@ -32,7 +32,7 @@
                 <embed type="application/x-shockwave-flash" src="{{ cms_config('site.web.url') }}/flash/traxplayer/traxplayer.swf"
                     name="traxplayer" quality="high" base="{{ cms_config('site.web.url') }}/flash/traxplayer/"
                     allowscriptaccess="always" menu="false" wmode="transparent"
-                    flashvars="songUrl={{ url('/') }}/myhabbo/trax_song/{{ $item->extra_data }}&amp;sampleUrl={{ url('') }}/dcr/sound/mp3/"
+                    flashvars="songUrl={{ url('/') }}/myhabbo/trax_song/{{ $item->extra_data }}&amp;sampleUrl={{ url('/') }}/dcr/sound/mp3/"
                     height="66" width="210" />
                 @endif
                 <div class="clear"></div>
